14f04d4644de970626a22a641d51c88f050cafc8
Diarrah <diarrahcasiano@gmail.com>
2020-07-28 21:35:43 -0700

    Functions for clipboard copy + setting copied variable

--- src/components/ShortenedURL.js
+++ src/components/ShortenedURL.js
@@ -9,12 +9,22 @@ const ShortenedURL = ({ dataFromAPI }) => {
         }
     }, [copied])
 
+    function copyLink(e) {
+        let newURL = e.target.parentNode.children[1].textContent;
+        window.navigator.clipboard.writeText(newURL);
+    }
+
+    const handleCopy = e => { // This is combining the 2 functions into 1 since you can't have 2 onClicks
+        setCopied(true);
+        copyLink(e)
+    }
+
     return(
-        <div className="item">
+        <div className="shortened__output">
             <span className="original__URL">{ dataFromAPI.url }</span>
             <span className="hash-id">{`https://rel.ink/${dataFromAPI.hashid}`}</span>
             <button
-                onClick={() => setCopied(true)}
+                onClick={handleCopy}
                 className={`btn--copy ${copied ? 'active' : ''}`}
             >
                 {copied ? 'Copied!' : 'Copy'}
