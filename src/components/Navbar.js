2690a1bd0fbdf6dd89a42fb2644e00f58162a6b0
Diarrah <diarrahcasiano@gmail.com>
2020-07-31 17:55:28 -0700

    Script added for mobile nav deployment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import Logo from '../images/logo.svg'
+import companyLogo from '../images/logo.svg'
 
 const Navbar = () => {
     const links = ['Features', 'Pricing', 'Resources'];
 
+    const modalOpen = (e) => {
+        let { body } = document;
+
+        e.target.parentElement.classList.toggle('deployed');
+        e.target.parentElement.classList.contains('deployed') 
+            ? body.style.overflowY = 'hidden' 
+            : body.style.overflowY = 'visible';
+    }
+
     return (
         <div className="navbar">
-            <img src={Logo} className="logo--main" alt="Company logo" />
+            <img src={companyLogo} className="logo logo--main" alt="Company logo" />
             <ul className="navbar__links navbar__links--main">  
                 {links.map((link, i) => (
                     <li className="navbar__link" key={i}>
@@ -23,6 +32,11 @@ const Navbar = () => {
                     <a href="#">Sign Up</a>
                 </li>
             </ul>
+
+            <a href="#" className="navbar--mobile" onClick={modalOpen}>
+                <i className="fa fa-bars hamburger" aria-hidden="true" />
+                <i className="fa fa-times close" aria-hidden="true" />
+            </a>
         </div>
     )
 }
